0bbae279a9edc4e73b6855aae9f711bba5314dd3
Jabborov Abduroziq <blogchikuz@gmail.com>
2023-06-02 04:53:06 +0500

    Complete

---
+++ README.md
@@ -0,0 +1,1 @@
+# asgn-scuba-diving
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/shop.css';
+import '../styles/components/travel.css';
+import '../styles/components/gallery.css';
+import '../styles/components/club.css';
+import '../styles/components/shopitems.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
